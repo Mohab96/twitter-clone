ff759e0281d29dd07a5714d447cfa8a75b95f87d
MostafaFouad0 <mm1471800@gmail.com>
2024-10-27 01:40:39 +0300

    fixed bugs
    fixed bug when the process.env.PORT is null

--- src/services/File Management/server.js
+++ src/services/File Management/server.js
@@ -8,11 +8,11 @@ const fileRoutes = require("./routes/filesRoutes");
 app.use("/api/file", fileRoutes);
 
 app.listen(process.env.PORT, () => {
-  console.log("[!] server is running on port:", process.env.PORT);
+  console.log("[!] server is running on port:", process.env.PORT || 3000);
   const uploadDir = "uploads";
   if (!fs.existsSync(uploadDir)) {
     console.log("[!] uploads folder does not exist, creating one...");
-    fs.mkdirSync(uploadDir, { recursive: true });
+    fs.mkdirSync(uploadDir);
   } else {
     console.log("[!] uploads folder already exists.");
   }
