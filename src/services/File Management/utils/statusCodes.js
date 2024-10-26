997c55e4f278152269ef36ade5252578cde7ed3d
MostafaFouad0 <mm1471800@gmail.com>
2024-10-27 01:39:37 +0300

    added more status codes

--- src/services/File Management/utils/statusCodes.js
+++ src/services/File Management/utils/statusCodes.js
@@ -4,6 +4,8 @@ const HTTP_201_CREATED = 201;
 const HTTP_500_INTERNAL_SERVER_ERROR = 500;
 const HTTP_401_UNAUTHORIZED = 401;
 const HTTP_404_NOT_FOUND = 404;
+const HTTP_202_Accepted = 202;
+const HTTP_102_Processing = 102;
 
 module.exports = {
   HTTP_200_SUCCESS,
@@ -12,4 +14,6 @@ module.exports = {
   HTTP_500_INTERNAL_SERVER_ERROR,
   HTTP_401_UNAUTHORIZED,
   HTTP_404_NOT_FOUND,
+  HTTP_202_Accepted,
+  HTTP_102_Processing,
 };
