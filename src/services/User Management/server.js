992f76dd7dc7fda3ed08f454b7aff3c93314e3f3
MostafaFouad0 <mm1471800@gmail.com>
2024-11-13 16:38:13 +0200

    fixed bug in server.js file

--- src/services/User Management/server.js
+++ src/services/User Management/server.js
@@ -6,6 +6,6 @@ app.use(express.json());
 
 app.use("/api/user", userRoutes);
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("[!] server is running on port:", process.env.PORT || 3000);
 });
